236ddd1aac0eae699e7ea29465db58c80ca96608
koonlookhin <krit_tipnuan@cmu.ac.th>
2022-09-22 12:00:29 +0700

    fix 3.6 v2

--- src/main/java/se331/rest/util/LabMapper.java
+++ src/main/java/se331/rest/util/LabMapper.java
@@ -1,11 +1,12 @@
 package se331.rest.util;
 
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 import se331.rest.entity.Event;
 import se331.rest.entity.EventDTO;
 
 import java.util.List;
-
+@Mapper
 public interface LabMapper {
     LabMapper INSTANCE = Mappers.getMapper(LabMapper.class);
     EventDTO getEventDto(Event event);
